168747fee168b655e042726d61bf5aecee7db513
Sankar Biswas <sankar.biswas@BHUKRK8056L.local>
2019-01-15 11:47:58 +0530

    initial commit

---
+++ src/main/scala/com/netflix/utilities/Cassandra.scala
@@ -0,0 +1,30 @@
+package com.netflix.utilities
+
+import com.datastax.driver.core.{ResultSet, Row}
+//import com.datastax.spark.connector.cql.CassandraConnector
+import com.datastax.spark.connector._
+import org.apache.spark.sql.{SQLContext, SparkSession}
+import org.apache.spark.{SparkConf, SparkContext}
+
+
+class Cassandra {
+  def main(args: Array[String]) {
+  val conf = new SparkConf().setAppName("APP_NAME")
+    .setMaster("local")
+    .set("spark.cassandra.connection.host", "localhost")
+    .set("spark.cassandra.auth.username", "")
+    .set("spark.cassandra.auth.password", "")
+
+  val spark: SparkSession = SparkSession.builder().config(conf).enableHiveSupport().getOrCreate()
+
+  val sc: SparkContext = spark.sparkContext
+
+  val data = sc.cassandraTable("datatable", "data")
+
+  val spark1 = SparkSession.builder().getOrCreate()
+  import spark1.implicits._
+
+  println(data.collect.foreach(println))
+  }
+
+}
